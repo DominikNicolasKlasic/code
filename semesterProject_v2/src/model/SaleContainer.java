d4857b01431ef9c7d6d5fb8369cb50b97880fc06
DominikNicolasKlasic <73468305+DominikNicolasKlasic@users.noreply.github.com>
2021-01-22 14:26:34 +0100

    Add files via upload

--- semesterProject_v2/src/model/SaleContainer.java
+++ semesterProject_v2/src/model/SaleContainer.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class SaleContainer {
 	private ArrayList<Sale> sales;
     private static SaleContainer saleContainer;
+    private int i;
     
     public SaleContainer() {
         sales = new ArrayList<Sale>();
@@ -17,11 +18,52 @@ public class SaleContainer {
         return saleContainer;
     }
     
-    public void createNewSale(int workerId) {
+    public void createNewSale(int orderId, int workerId) {
         int i = sales.size();
         Sale s = new Sale(i, workerId);
         sales.add(s);
         System.out.println("The sale: " + s + " has been created.");
+    }
+    
+    public void addItem() {
+    }
+    
+    public void findSaleByOrderId(int orderId) {
+    	int match = 0;
+
+        for(Sale s : sales) {
+            if (orderId == s.getOrderId()) {
+            	System.out.println("***********************");
+				System.out.println("Sale order ID: " + s.getOrderId());
+				System.out.println("Sale worker ID: " + s.getWorkerId());
+				System.out.println("Sale size: " + s.getSize());
+				System.out.println("Sale price: " + s.getTotalPrice());
+				System.out.println("***********************");
+                match = 1;
+                i++;
+
+            }
+        }
+        if (match == 0) {
+            System.out.println("Something went wrong..");
+        }
+    }
+    
+    public void endSalebyOrderId(int orderId) {
+    	int match = 0;
+
+        for(Sale s : sales) {
+            if (orderId == s.getOrderId()) {
+                sales.remove(s);
+            	System.out.println("The sale with the order ID: " + "'" + s.getOrderId() + "'" + " has ended.");
+                match = 1;
+                i++;
+
+            }
+        }
+        if (match == 0) {
+            System.out.println("Something went wrong..");
+        }
     }
     
     public int getSize() {

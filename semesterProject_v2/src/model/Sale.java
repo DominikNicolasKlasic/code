dd8f3e96c66394cdac0ed42fb84112a1d63f589a
DominikNicolasKlasic <73468305+DominikNicolasKlasic@users.noreply.github.com>
2021-01-22 13:41:38 +0100

    Add files via upload

---
+++ semesterProject_v2/src/model/Sale.java
@@ -0,0 +1,91 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Sale {
+	private ArrayList<ShoppingCart> cart;
+	private int orderId;
+	private double price;
+	private int workerId;
+	private boolean active;
+	
+	public Sale(int orderId, int workerId) {
+		cart = new ArrayList<>();
+		this.setOrderId(orderId);
+		this.setWorkerId(workerId);
+		this.price = 0;
+		active = true;
+	}
+	
+	public void addItem(Product p) {
+		boolean alreadyExists = false;
+		int i = 0;
+		if(isActive()) {
+			if(alreadyExists) {
+				cart.get(i).getAmountOfItems();
+				System.out.println("The cart already exists. And consists of " + cart.get(i).getAmountOfItems() + " items.");
+			} else {
+				ShoppingCart sc = new ShoppingCart();
+				cart.add(sc);
+				System.out.println("New shopping cart has been created.");
+			}
+		} else {
+			System.out.println("Something went wrong.");
+		}
+	}
+	
+	public void addItemWithDiscount(Product p, double discount) {
+		boolean alreadyExists = false;
+		int i = 0;
+		if(isActive()) {
+			if(alreadyExists) {
+				cart.get(i).getAmountOfItems();
+				cart.get(i).setDiscountPrice(discount);
+				System.out.println("The shopping cart already exists.")
+			} else {
+				ShoppingCart sc = new ShoppingCart();
+				cart.add(sc);
+				sc.setDiscountPrice(discount);
+				System.out.println("The shopping cart has been created and the discount applied.");
+				
+			}
+		} else {
+			System.out.println("Something went wrong...");
+		}
+	}
+	
+	public void printTotalPrice() {
+        double  tp = 0;
+        if(cart.size() != 0) {
+            for(ShoppingCart sc : cart) {
+                tp += sc.getCartPrice();
+            }
+        }
+        price = tp;
+        System.out.println("The price is: " + price);
+    }
+	
+	public void endSale() {
+        active = false;
+    }
+	
+	public boolean isActive() {
+	    return active;
+	}
+	 
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	
+	public int getOrderId() {
+		return orderId;
+	}
+	
+	public void setWorkerId(int workerId) {
+		this.workerId = workerId;
+	}
+	
+	public int getWorkerId() {
+		return workerId;
+	}
+}
